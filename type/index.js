1ed65c17172d65e1383aeef090cfc7042420f758
scx567888 <scx567888@outlook.com>
2023-11-14 15:48:01 +0800

    0.0.5 (#4)

--- type/index.js
+++ type/index.js
@@ -2,6 +2,7 @@ export * from "./ali-oss.js";
 export * from "./auth-fetch.js";
 export * from "./crud-context.js";
 export * from "./crud-context-vue-installer.js";
+export * from "./duration-format.js";
 export * from "./easy-options.js";
 export * from "./scx-auth.js";
 export * from "./scx-router.js";
