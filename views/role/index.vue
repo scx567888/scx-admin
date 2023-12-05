a8394b7bb02d22eb350ba8485c849729b6603870
scx567888 <scx567888@outlook.com>
2023-12-05 15:56:51 +0800

    dev

--- views/role/index.vue
+++ views/role/index.vue
@@ -1,20 +1,49 @@
 <template>
     <div class="scx-role">
-        
+
         <div class="left-page">
-            <role-tree/>
+            <role-tree ref="roleTreeRef" :readonly="false" :show-user-count="false" @node-click="treeNodeClick"/>
         </div>
-        
+
         <div class="right-page">
-            <role-perms/>
+            <role-perms :data="nowRole" @on-update="onUpdate"/>
         </div>
-        
+
     </div>
 </template>
-<script setup>
 
+<script setup>
+import {ref} from "vue";
 import RoleTree from "./role-tree.vue";
 import RolePerms from "./role-perms.vue";
+import {deepCopy} from "scx-ui";
+
+//左侧角色树的 ref
+const roleTreeRef = ref();
+
+//当前选中角色
+const nowRole = ref({
+    id: -1,
+    name: "",
+    children: [],
+    pagePerms: [],
+    perms: [],
+    pageElementPerms: [],
+    apiPerms: []
+});
+
+nowRole.value = null;
+
+//点击树
+function treeNodeClick(data) {
+    //深拷贝一下点击的节点并赋值给 nowRole
+    nowRole.value = deepCopy(data);
+}
+
+function onUpdate() {
+    roleTreeRef.value.refreshListData();
+}
+
 </script>
 <style scoped>
 .scx-role {
